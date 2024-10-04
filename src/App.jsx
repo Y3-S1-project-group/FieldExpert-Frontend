b46b5dcec4f90d3107710aea4325833e0c4cd952
AVJayasundara <avimod736@gmail.com>
2024-10-04 23:48:50 +0530

    FieldExpert-Frontend final
    FieldExpert-Frontend final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 // Importing page routes
 import Home from './pages/Home';
 import PestDetection from './pages/PestDetection';
+import Fertilizer from './pages/Fertilizer';
+import CultivatedLandAreaChart from './pages/CultivatedLandAreaChart';
+
+
+
+
 
 function App() {
   return (
@@ -12,7 +17,13 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/pestDetect' element={<PestDetection />} />
+          <Route path='/fertilizer' element={<Fertilizer />} />
+          <Route path='/cultivatedLandAreaChart' element={<CultivatedLandAreaChart />} />
         </Routes>
+
+        
+
+        
       </main>
     </Router>
   );
