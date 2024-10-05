095364498b82b881b9bce45346bc0600cb01a6ca
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-10-05 16:15:25 +0530

    Home Styles done. Add the Footer.

--- src/pages/AdminInventory/AdminInventory.jsx
+++ src/pages/AdminInventory/AdminInventory.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 import DisplayInventory from '../../components/DisplayInventory/DisplayInventories'
 import InventoryReport from '../../components/InventoryReport/InventoryReport';
 import AddInventory from '../../components/AddInventory/AddInventory';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 const AdminInventory = () => {
   return (
     <>
-      
+      <Navbar />
 
-      <div className="ml-2 mt-8 px-2">
+      <div className="px-2 mt-40 ml-2">
         
         <InventoryReport />
         <AddInventory />
@@ -18,6 +20,7 @@ const AdminInventory = () => {
         <br />
         <DisplayInventory />
       </div>
+      <Footer />
     </>
   );
 }
